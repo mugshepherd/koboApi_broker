ef933af127234bffd0ef6902e49d13ca28769e83
Toby <toby.schaeffer@gmail.com>
2016-06-30 18:24:19 +0300

    updated seeds file, model

--- app/models/kobo_api.rb
+++ app/models/kobo_api.rb
@@ -5,7 +5,7 @@ class KoboApi < ActiveRecord::Base
 	USERNAME = ENV['KOBO_API_USER']
 	PASSWORD = ENV['KOBO_API_PWD']
 	SURVEY = ENV['KOBO_API_SURVEY']
-	API_BASE_URL = "https://kc.kobotoolbox.org/api/v1/"
+	API_BASE_URL = ENV['API_BASE_URL']
 	# API_BASE_URL = "https://sg.smap.com.au/api/v1"
 
 	def self.get_api_info
