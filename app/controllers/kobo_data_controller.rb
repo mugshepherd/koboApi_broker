1bc539c6b6accd9c412cba0dced26bcbdb0be8b6
Toby <toby.schaeffer@gmail.com>
2016-07-06 02:27:45 +0300

    updated controller with new month and year fields

--- app/controllers/kobo_data_controller.rb
+++ app/controllers/kobo_data_controller.rb
@@ -9,53 +9,25 @@ def index
 			puts "kobo_info: #{kobo_info.inspect}"
 		  # begin
 		  	# #this pulls in new records that have been added to the api and adds them to the database.  it doesn't pull in records from the api that are already in the db
+
+		  #note use of .split(",").map method for geolocation, if used	
 		   #  KoboApi.find_or_create_by(lemurs_quantity: kobo_info['lemurs_quantity'], month_and_year:  Date.parse(kobo_info['month_and_year']), _geolocation: kobo_info['_geolocation'].split(",").map { |s| s.to_i }, lemur_category: kobo_info['lemur_category'], location_admin1: kobo_info['location_admin1'], location_admin2: kobo_info['location_admin2'])
-		   date = if kobo_info['month_and_year']
-		   		Date.parse(kobo_info['month_and_year'])
-		   else
-		   		nil
-		   end
-
-
-
-		   # quantity = if kobo_info['lemurs_quantity']
-		   # 		kobo_info['lemurs_quantity']
-		   # 	else 
+		   # date = if kobo_info['month_and_year']
+		   # 		Date.parse(kobo_info['month_and_year'])
+		   # else
 		   # 		nil
-		   # 	end
-
-				# category = if kobo_info['lemur_category']
-		  #  		kobo_info['lemur_category']
-		  #  	else 
-		  #  		nil
-		  #  	end
-
-				# admin1 = if kobo_info['location_admin1']
-		  #  		kobo_info['location_admin1']
-		  #  	else 
-		  #  		nil
-		  #  	end
-				
-				# admin2 = if kobo_info['location_admin2']
-		  #  		kobo_info['location_admin2']
-		  #  	else 
-		  #  		nil
-		  #  	end
+		   # end
 
 
-		    KoboApi.find_or_create_by(record_id: kobo_info['_id'], lemurs_quantity: kobo_info['lemurs_quantity'], month_and_year: date, lemur_category: kobo_info['lemur_category'], location_admin1: kobo_info['location_admin1'], location_admin2: kobo_info['location_admin2'])
-# 
- # KoboApi.create(lemurs_quantity: kobo_info['lemurs_quantity'], month_and_year: date, lemur_category: kobo_info['lemur_category'], location_admin1: kobo_info['location_admin1'], location_admin2: kobo_info['location_admin2'])
-		    # KoboApi.create(lemurs_quantity: quantity, month_and_year: date, lemur_category: category, location_admin1: admin1, location_admin2: admin2)
+		    KoboApi.find_or_create_by(record_id: kobo_info['_id'], lemurs_quantity: kobo_info['lemurs_quantity'], sighting_month: kobo_info['sighting_month'], sighting_year: kobo_info['sighting_year'], lemur_category: kobo_info['lemur_category'], location_admin1: kobo_info['location_admin1'], location_admin2: kobo_info['location_admin2'])
 
-
-		  #some of my records don't have valid date values, so `rescue` is added to keep rails from returning a `notfound` page.  quick tutorial here:  http://www.rubytutorial.io/rails-rescue_from/
+		  #if dates don't have valid values, use `rescue` to keep rails from returning a `notfound` page.  quick tutorial here:  http://www.rubytutorial.io/rails-rescue_from/
 		  # rescue 
 		  #   puts "rescued"
 		  # end
 		end
 		#i needed to update my render logic.  see addition of `each_serializer` here.  @kobo_info has also changed to KoboApi.all as well.
-    render(
+    render(	
       json: KoboApi.all,
       each_serializer: KoboApiSerializer
     )
